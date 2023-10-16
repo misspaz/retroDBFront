74e151c8e1ffa901270d6fbca97fb1479598a7a4
Betulioo <luis.nunez@bootcamp-upgrade.com>
2023-10-16 14:04:17 +0200

    search

--- HOME/script-home.js
+++ HOME/script-home.js
@@ -41,31 +41,65 @@ const localgetFetch = async () => {
   return result;
 };
 
-// const drawInput$$ = (arrayGames) => {
-//     const input$$ = document.querySelector("input");
+const agarraInput$$ = (arrayGames) => {
+    const input$$ = document.querySelector("input");
   
-//     input$$.addEventListener("input", () =>
-//       searchGames(input$$.value, arrayGames)
-//     );
-//   };
+    input$$.addEventListener("input", () =>
+      searchGames(input$$.value, arrayGames)
+    );
+  };
 
   
-//   const searchGames = (filtro, games) => {
-//     let gamesSearched = games.filter((game) =>
-//       game.title.toLowerCase().includes(filtro.toLowerCase())
-//     );
+  const searchGames = (filtro, games) => {
+    let gamesSearched = games.filter((game) =>
+      game.title.toLowerCase().includes(filtro.toLowerCase())
+    );
   
-//     drawPokemons(pokemonsFiltrados);
-//   };
+    pintaGames(gamesSearched);
+  };
 
-// const drawGames = () => {
+const pintaGames = (gamesSearched) => {
+  const sectiondiv$$ = document.querySelector(".videogames")
+  sectiondiv$$.innerHTML=""
+  for (const videogame of gamesSearched) {
+    const div$$ = document.createElement("div");
+    div$$.classList.add("videogame-bg");
+    const title$$ = document.createElement("h2");
+    const year$$ = document.createElement("p");
+    const genre$$ = document.createElement("p");
+    const country$$ = document.createElement("p");
+    const image$$ = document.createElement("img");
 
+    image$$.setAttribute("src", videogame.image);
+    image$$.classList.add("game-image");
+    country$$.textContent = videogame.country;
+    country$$.classList.add("info-games");
+    genre$$.textContent = videogame.genre;
+    genre$$.classList.add("info-games");
+    year$$.textContent = videogame.year;
+    year$$.classList.add("info-games");
+    title$$.textContent = videogame.title;
+    title$$.classList.add("info-games");
+    title$$.classList.add("game-title");
+    div$$.appendChild(title$$);
+    div$$.appendChild(genre$$);
+    div$$.appendChild(year$$);
+    div$$.appendChild(country$$);
+    div$$.appendChild(image$$);
+   
+     const section$$ = document.querySelector(".videogames");
+    section$$.appendChild(div$$);
     
-// }
+    document.body.appendChild(section$$);
+  }
+  return
+    
+}
 
 
 const init = async () => {
-    localgetFetch();
+   const arrayGames = await localgetFetch();
+    agarraInput$$(arrayGames);
     // const arrayGames = await localgetFetch()
 //   console.log(arrayGames);
 //   drawInput$$(arrayGames);
